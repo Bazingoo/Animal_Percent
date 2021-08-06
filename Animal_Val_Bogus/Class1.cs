f1ba70351e3f2eb2864d2bf0095df413d6086ff8
Sergii <http.serg.com@ukr.net>
2021-08-06 12:55:27 +0300

    Test

---
+++ Animal_Val_Bogus/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal_Percent
+{
+    class Class1
+    {
+
+        /*int count_proc = 5; // количество процентов, после которых идет переход
+        int steps = 100 / count_proc + 1; // Получаем количество шагов
+        Console.WriteLine("Типа установка");
+            Console.WriteLine("[{0}]", new String(' ', steps)); // Печатаем шаблон
+
+            Console.SetCursorPosition(0, Console.CursorTop - 1); // Устанавливаем начальную позицию
+            for (int process = 0; process<steps; process++)
+            {
+                Console.SetCursorPosition(process + 1, Console.CursorTop); // устанавливаем позицию
+                Console.Write("|");
+
+                Console.SetCursorPosition(steps + 3, Console.CursorTop);
+                Console.Write("{0}%", process* count_proc); // рисуем проценты прогресса
+
+        Random r = new Random();
+        System.Threading.Thread.Sleep(r.Next(40, 600));// Симулируем нагрузку
+            }
+    Console.WriteLine(); // Переходим на новую строку
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Готово!");
+            Console.ReadLine();*/
+    }
+}
